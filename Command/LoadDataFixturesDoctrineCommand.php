59a7b03af68acb5eb0b2c57acf31fb5688155f66
Albert Jessurum <ajessu@gmail.com>
2011-04-23 09:31:38 +0200

    Fixes #4 - Synched data fixture command between ORM and ODM Bundles

--- Command/LoadDataFixturesDoctrineCommand.php
+++ Command/LoadDataFixturesDoctrineCommand.php
@@ -41,7 +41,7 @@ class LoadDataFixturesDoctrineCommand extends DoctrineCommand
             ->setName('doctrine:fixtures:load')
             ->setDescription('Load data fixtures to your database.')
             ->addOption('fixtures', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'The directory or file to load data fixtures from.')
-            ->addOption('append', null, InputOption::VALUE_OPTIONAL, 'Whether or not to append the data fixtures.', false)
+            ->addOption('append', null, InputOption::VALUE_NONE, 'Append the data fixtures instead of flushing the database first.')
             ->addOption('em', null, InputOption::VALUE_REQUIRED, 'The entity manager to use for this command.')
             ->setHelp(<<<EOT
 The <info>doctrine:fixtures:load</info> command loads data fixtures from your bundles:

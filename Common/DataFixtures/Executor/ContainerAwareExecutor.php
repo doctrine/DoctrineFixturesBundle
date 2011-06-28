c513ea2653c923fc8679ff4e755ae63eb3f300d6
jaugustin <jeremie.augustin@pixel-cookers.com>
2011-06-28 17:14:48 +0200

    Added a ContainerAwareExecutor

---
+++ Common/DataFixtures/Executor/ContainerAwareExecutor.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Symfony\Bundle\DoctrineFixturesBundle\Common\DataFixtures\Executor;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Doctrine\Common\DataFixtures\Purger\PurgerInterface;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\ORM\EntityManager;
+
+class ContainerAwareExecutor extends ORMExecutor
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Default constructor.
+     *
+     * @param EntityManager $em
+     * @param PurgerInterface $purger
+     * @param ContainerInterface $container
+     */
+    public function __construct(EntityManager $em, PurgerInterface $purger, ContainerInterface $container)
+    {
+        parent::__construct($em, $purger);
+
+        $this->container = $container;
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    /**
+     * @see Doctrine\Common\DataFixtures\Executor.AbstractExecutor::load()
+     * {@inheritdoc}
+     */
+    public function load($manager, FixtureInterface $fixture)
+    {
+        if ($fixture instanceof ContainerAwareInterface) {
+            $fixture->setContainer($this->getContainer());
+        }
+
+        parent::load($manager, $fixture);
+    }
+}

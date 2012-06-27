76b8e639d3fc3333fb15a19572be6dbbde894496
Christophe Coevoet <stof@notk.org>
2012-06-28 00:51:54 +0200

    Removed the custom fixture loader as it is now in the bridge

--- Command/LoadDataFixturesDoctrineCommand.php
+++ Command/LoadDataFixturesDoctrineCommand.php
@@ -18,7 +18,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
-use Doctrine\Bundle\FixturesBundle\Common\DataFixtures\Loader as DataFixturesLoader;
+use Symfony\Bridge\Doctrine\DataFixtures\ContainerAwareLoader as DataFixturesLoader;
 use Doctrine\Bundle\DoctrineBundle\Command\DoctrineCommand;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;

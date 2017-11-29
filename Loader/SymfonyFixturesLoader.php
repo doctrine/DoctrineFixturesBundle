6521e170819c705e3c9e7d14cb9c334e6355c91c
Ruud Kamphuis <ruudk@users.noreply.github.com>
2017-11-29 12:20:04 +0100

    Fix strict comparison in DependentFixtureInterface
    See https://github.com/doctrine/DoctrineFixturesBundle/pull/216#issuecomment-349077657

--- Loader/SymfonyFixturesLoader.php
+++ Loader/SymfonyFixturesLoader.php
@@ -40,14 +40,9 @@ final class SymfonyFixturesLoader extends ContainerAwareLoader
     public function addFixture(FixtureInterface $fixture)
     {
         $class = get_class($fixture);
-        if (isset($this->loadedFixtures[$class]) && $this->loadedFixtures[$class] !== $fixture) {
-            throw new \LogicException(sprintf(
-                'The "%s" fixture class is already loaded and instantiated. It is not possible to add a new instance of this fixture. Upgrade to "doctrine/data-fixtures" version 1.3 or higher to support this.',
-                $class
-            ));
+        if (!isset($this->loadedFixtures[$class])) {
+            $this->loadedFixtures[$class] = $fixture;
         }
-
-        $this->loadedFixtures[$class] = $fixture;
 
         // see https://github.com/doctrine/data-fixtures/pull/274
         // this is to give a clear error if you do not have this version

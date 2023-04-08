b02abd60b77c8346f26103debf380032ea28c5fd
Phil E. Taylor <phil@phil-taylor.com>
2023-04-08 20:40:29 +0100

    add "void" as a native return type declaration

--- DoctrineFixturesBundle.php
+++ DoctrineFixturesBundle.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DoctrineFixturesBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new FixturesCompilerPass());
         $container->addCompilerPass(new PurgerFactoryCompilerPass());
